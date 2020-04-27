3a1f684f08eaba5ed97073e83f1ef44bc5dc561b
Ki-su, Kim <rabbitthief@naver.com>
2020-04-27 20:32:03 +0900

    change db location

--- nodejs/db/test.js
+++ nodejs/db/test.js
@@ -1,11 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // open the database
-let db = new sqlite3.Database('./bbantheon.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./db/bbantheon.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
-  console.log('Connected to the chinook database.');
+  console.log('Connected to the bbantheon database.');
 });
 
 db.serialize(() => {
